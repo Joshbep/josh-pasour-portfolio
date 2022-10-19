e02db9db3ecb62ea61eb01032aae40293938a99e
jbep <joshuabemartin@gmail.com>
2022-10-19 15:04:29 -0400

    small update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "206baf8d401499ca"; }
+/******/ 		__webpack_require__.h = function() { return "d14606798b454193"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
