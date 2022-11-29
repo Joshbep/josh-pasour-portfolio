529216e028ded6837e70e966e491374549c5fe5a
jbep <joshuabemartin@gmail.com>
2022-11-28 19:58:14 -0500

    updated about

--- components/About.jsx
+++ components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className='col-span-2'>
           <p className='uppercase text-xl tracking-widest text-[#4F7942]'>About</p>
           <h2 className='py-4'>Who I am</h2>
-          <p className='py-2 text-gray-600'>I am a developing software engineer and I completed a Bootcamp at General Assembly. As someone who loves to constantly problem solve and continuously evolve their skillset, software engineering feels like a dream.</p>
+          <p className='py-2 text-gray-600'>I am a developing software engineer and I completed a Bootcamp at General Assembly! As someone who loves to constantly problem solve and continuously evolve their skillset, software engineering feels like a dream.</p>
           <p className='py-2 text-gray-600'>I worked in marketing before studying to become an engineer. The marketing industry taught me how to break larger problems into smaller chunks and to adapt and grow with each project. With $10,000,000+ spent in marketing with startups, I learned how to effectively manage my time. I also got to learn from and observe our web development team with every new client. The more time that passed by, the more I realized I had a burning passion for software development and that has led me here.</p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
